8cb5b1bd8c76ca610f9b3d59f0b81908c670d08c
Daniel Barboza <barbozadaniel1994@gmail.com>
2024-04-25 11:52:55 +0000

    Added support for EfficientNet including pre-trained

---
+++ code/models/enet_models.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from efficientnet_pytorch import model as enet
+
+class ENetModel(nn.Module):
+    def __init__(self, backbone, c_out=5):
+        super(ENetModel, self).__init__()
+        self.enet = enet.EfficientNet.from_pretrained(backbone)
+
+        self.myfc = nn.Linear(self.enet._fc.in_features, c_out)
+        self.enet._fc = nn.Identity()
+
+    def extract(self, x):
+        return self.enet(x)
+
+    def forward(self, x):
+        x = self.extract(x)
+        x = self.myfc(x)
+        return x
